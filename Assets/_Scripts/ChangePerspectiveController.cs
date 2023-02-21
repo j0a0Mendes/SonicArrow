4539be79676b72c44c11de599e51dc28589d2460
Joãooo <mm2000_@hotmail.com>
2023-02-21 11:08:36 +0000

    upd: change perspective bug fixed

--- Assets/_Scripts/ChangePerspectiveController.cs
+++ Assets/_Scripts/ChangePerspectiveController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class ChangePerspectiveController : MonoBehaviour
@@ -37,7 +38,9 @@ public class ChangePerspectiveController : MonoBehaviour
             if(changePerspectiveTrigger & !firstPerspective){
                 changePerspectiveTrigger = false;
                 //controllerStand.transform.position = new Vector3(-9.5f,0.3f,5.5f);
-                xrorigin.position = new Vector3(-20.5f,0.3f,5.5f);
+                //xrorigin.position = new Vector3(-15f,-1.28f,-5.42f);
+                xrorigin.position = new Vector3(-15.273f, 0.082f, -5.23f);
+                //xrorigin.Rotate(0f, 89.989f, 0f);
                 firstPerspective = true;
             }
         }
